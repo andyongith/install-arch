48f0ba37a179f85a55d51a12fd8c113c759d9aa7
Andy <144360327+andyongith@users.noreply.github.com>
2024-03-17 12:47:30 +0530

    Just some updates

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+pkg_i_use=(man-db git nerd-fonts $(pacman -Ssq noto-fonts) firefox kitty tmux ranger python-pillow tldr htop neofetch github-cli)
+
 ### Taking User Inputs
 #
 function choose_from_menu()
@@ -51,15 +53,16 @@ function choose_from_menu()
 }
 
 function show_all_vars() {
-  echo " ESP         : $ESP"
-  echo " ROOT        : $ROOT"
-  echo " USER_NAME   : $USER_NAME"
-  echo " PASSWORD    : $PASSWORD"
-  echo " RTPASSWD    : $RTPASSWD"
-  echo " HOST_NAME   : $HOST_NAME"
-  echo " display_mgr : $display_mgr"
-  echo " setup_pkg   : ${setup_pkg[@]}"
-  echo " extra_pkg   : ${extra_pkg[@]}"
+  echo " ESP           : $ESP"
+  echo " ROOT          : $ROOT"
+  echo " USER_NAME     : $USER_NAME"
+  echo " PASSWORD      : $PASSWORD"
+  echo " RTPASSWD      : $RTPASSWD"
+  echo " HOST_NAME     : $HOST_NAME"
+  echo " display_mgr   : $display_mgr"
+  echo " setup_pkg     : ${setup_pkg[@]}"
+  echo " extra_pkg     : ${extra_pkg[@]}"
+  echo " BOOTLOADER_ID : $BOOTLOADER_ID"
 }
 
 function show_page {
@@ -108,35 +111,53 @@ select_user_and_hostname
 
 function select_gui_setup() {
   display_mgr=
-  setup_pkg=
+  setup_pkg=()
   extra_pkg=
   show_page
-  choose_from_menu "Choose your GUI setup" setup_pkg "xorg gnome gnome-extra gdm" "plasma-meta kde-applications-meta sddm" "Minimal"
+  choose_from_menu "Choose your GUI setup" setup_pkg "xorg gnome gnome-extra gdm" "xorg plasma-meta kde-applications-meta sddm" "Minimal"
   case $REPLY in
     1) display_mgr=gdm
       ;;
     2) display_mgr=sddm
       ;;
-    *) setup_pkg=""
+    *) setup_pkg=()
       ;;
   esac
   setup_pkg=($setup_pkg)
   echo
+  echo "Some additional packages that I use:" 
+  echo ${pkg_i_use[@]}
+  echo
+  choose_from_menu "Do you want these packages too?" install_my_pkg "yes" "no"
+  echo
   echo "Extra packages to install(separated by white space): "
   read extra_pkg
+  [[ $install_my_pkg == "yes" ]] && extra_pkg=(${extra_pkg[@]} ${pkg_i_use[@]})
 }
 select_gui_setup
+
+BOOTLOADER_ID="Arch"
+function select_bootloader_id() {
+  show_page
+  BOOTLOADER_ID=
+  echo "Enter bootloader-id: "
+  while [[ $BOOTLOADER_ID == "" ]]; do read BOOTLOADER_ID; done
+}
 
 while true
 do
   show_page
-  choose_from_menu "Do you need any changes?" change_to_make "Partitions" "User and hostname" "GUI setup" "No, continue"
+  choose_from_menu "Do you need any changes?" change_to_make "Partitions" "User and hostname" "GUI setup" "bootloader-id" "Abort" "No, continue"
   case $REPLY in
     1) select_partitions
       ;;
     2) select_user_and_hostname
       ;;
     3) select_gui_setup
+      ;;
+    4) select_bootloader_id
+      ;;
+    5) exit 1
       ;;
     *) break
       ;;
@@ -147,7 +168,7 @@ show_page
 
 ### Installation
 #
-mkfs.vfat -F32 -n "ESP" $ESP
+mkfs.vfat -F32 -n "Arch-ESP" $ESP
 mkfs.ext4 -L "Arch-root" $ROOT
 
 mount $ROOT /mnt/
@@ -180,7 +201,7 @@ pacstrap /mnt/ \
   networkmanager \
   vi vim \
   reflector \
-  man-db git ${setup_pkg[@]} ${extra_pkg[@]} \
+  ${setup_pkg[@]} ${extra_pkg[@]} \
   --noconfirm --needed
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 
@@ -206,7 +227,7 @@ grep '# %sudo' /etc/sudoers | grep -q -v 'NOPASSWD' &&
 sed -i \"\$(grep -n '# %sudo' /etc/sudoers | grep -v 'NOPASSWD' | cut -d: -f1)s/# //\" /etc/sudoers
 
 sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
-grub-install --bootloader-id=Arch --efi-directory=/boot/efi/ --target=x86_64-efi
+grub-install --bootloader-id=${BOOTLOADER_ID} --efi-directory=/boot/efi/ --target=x86_64-efi
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo \"${RTPASSWD}\n${RTPASSWD}\n\" | passwd
@@ -229,3 +250,9 @@ rm /mnt/afterscript.sh
 
 umount /mnt/boot/efi
 umount /mnt
+
+echo
+echo "I don't know why but for some reasons os-prober is not detecting windows OS"
+echo "So, you've to run the following command after restart"
+echo "            sudo grub-mkconfig -o /boot/grub/grub.cfg"
+echo "Now, simply restart the pc"
